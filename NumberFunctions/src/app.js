3f9979738d63b734b5ed8085f8b76595f86c5d46
Tony Brix <Tony@Brix.ninja>
2016-04-07 09:59:34 -0500

    chore(build): add build tools
    add semantic-release, .travis.yml, and commitizen

---
+++ NumberFunctions/src/app.js
@@ -0,0 +1,143 @@
+﻿function defProp(prop, def) {
+    if (typeof prop === "undefined") {
+        return def;
+    }
+    return prop;
+}
+function regexpEscape(s) {
+    return s.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/, "\\$&");
+}
+exports.toNumber = function (string, options) {
+    if (typeof string === "number") {
+        return string;
+    }
+    if (typeof string !== "string") {
+        return NaN;
+    }
+    options = defProp(options, {});
+    decimalMark = defProp(options.decimalMark, ".");
+    regexpDecimalMark = regexpEscape(decimalMark);
+    var n = string.trim();
+    var negative = n.match(/^\(.*\)$|^-/);//negative if matches '(...)' or '-...'
+    var getNumberRegexp = new RegExp("[^\\d" + regexpDecimalMark + "]|" + regexpDecimalMark + "(?=.*" + regexpDecimalMark + ")|^\\D*" + regexpDecimalMark + "\\D*$", "g");
+    n = n.replace(getNumberRegexp, "").replace(decimalMark, ".");//remove all except digits and last dot
+    if (n === "") {
+        n = NaN;
+    } else if (negative) {
+        n = -n;
+    }
+    return Number(n);
+};
+
+exports.toClean = function (number, options) {// 1.500000 -> 1.5; 1.0000 -> 1
+    if (typeof number !== "number") {
+        number = this.toNumber(number, options);
+    }
+    if (isNaN(number)) {
+        return "NaN";
+    }
+    
+    options = defProp(options, {});
+    decimalMark = defProp(options.decimalMark, ".");
+    thousandSeperator = defProp(options.thousandSeperator, ",");
+    maxPrecision = defProp(options.maxPrecision, 20);
+    minPrecision = defProp(options.minPrecision, 0);
+    
+    maxPrecision = (maxPrecision > 20 ? 20 : (maxPrecision < 0 ? 0 : maxPrecision));
+    minPrecision = (minPrecision < 0 ? 0 : (minPrecision > 20 ? 20 : minPrecision));
+    if (minPrecision > maxPrecision) {
+        var temp = maxPrecision;
+        maxPrecision = minPrecision;
+        minPrecision = temp;
+    }
+    var n = number;
+    
+    //limit to maxPrecision
+    n = String(+n.toFixed(maxPrecision));
+    var dotIndex = n.lastIndexOf(".");
+    if (dotIndex > -1) {
+        n = n.slice(0, dotIndex) + decimalMark + n.slice(dotIndex + 1);
+    } else {
+        n += decimalMark;
+    }
+    //limit to minPrecision
+    if (minPrecision > 0) {
+        var numZeros;
+        if (dotIndex > -1) {
+            numZeros = minPrecision - (n.length - dotIndex - 1);
+        } else {
+            numZeros = minPrecision;
+        }
+        for (var i = 0; i < numZeros; i++) {
+            n += "0";
+        }
+    }
+    regexpDecimalMark = regexpEscape(decimalMark);
+    thousandSeperatorRegexp = new RegExp("\\d(?=(\\d{3})+" + regexpDecimalMark + ")", "g");
+    trimRegexp = new RegExp(regexpDecimalMark + "$");
+    n = n.replace(thousandSeperatorRegexp, "$&" + thousandSeperator).replace(trimRegexp, "");
+    
+    return n;
+};
+
+//modified from http://stackoverflow.com/a/149099/806777
+exports.toMoney = function (number, options) { // -1234.56 -> ($1,234.56)
+    if (typeof number !== "number") {
+        number = this.toNumber(number, options);
+    }
+    if (isNaN(number)) {
+        return "NaN";
+    }
+    if (number === Infinity) {
+        return "Infinity";
+    }
+    if (number === -Infinity) {
+        return "(Infinity)";
+    }
+    options = defProp(options, {});
+    decimalMark = defProp(options.decimalMark, ".");
+    thousandSeperator = defProp(options.thousandSeperator, ",");
+    maxPrecision = defProp(options.maxPrecision, 2);
+    minPrecision = defProp(options.minPrecision, 2);
+    if (maxPrecision < 2 && typeof options.minPrecision === "undefined") {
+        minPrecision = maxPrecision;
+    }
+    if (minPrecision > 2 && typeof options.maxPrecision === "undefined") {
+        maxPrecision = minPrecision;
+    }
+    symbol = defProp(options.symbol, "$");
+    symbolBehind = defProp(options.symbolBehind, false);
+    
+    var negative = number < 0;
+    var n = Math.abs(number);
+
+    n = this.toClean(n, {
+        decimalMark: decimalMark,
+        thousandSeperator: thousandSeperator,
+        maxPrecision: maxPrecision,
+        minPrecision: minPrecision
+    });
+
+    n = (symbolBehind? n + " " + symbol: symbol + n);
+    if (negative) {
+        n = "(" + n + ")";
+    }
+    return n;
+};
+
+exports.toClosest = function (number, roundTo) {
+    if (typeof number !== "number") {
+        number = this.toNumber(number);
+    }
+    if (isNaN(number)) {
+        return NaN;
+    }
+    if (number === Infinity || number === -Infinity) {
+        return number;
+    }
+    if (isNaN(roundTo)) {
+        return NaN;
+    }
+    var n = Math.round(number / roundTo) * roundTo;
+    return n;
+};
---
+++ NumberFunctions/src/app.test.js
@@ -0,0 +1,172 @@
+﻿var assert = require('assert');
+var ns = require('./app.js');
+
+describe('toNumber', function () {
+    it('Positive Number', function () {
+        assert.strictEqual(ns.toNumber(2), 2);
+    });
+    it('Negative Number', function () {
+        assert.strictEqual(ns.toNumber(-2), -2);
+    });
+    it('NaN', function () {
+        assert.ok(isNaN(ns.toNumber(NaN)));
+    });
+    it('Infinity', function () {
+        assert.strictEqual(ns.toNumber(Infinity), Infinity);
+    });
+    it('-Infinity', function () {
+        assert.strictEqual(ns.toNumber(-Infinity), -Infinity);
+    });
+    it('Positive String', function () {
+        assert.strictEqual(ns.toNumber("2"), 2);
+    });
+    it('Negative - String', function () {
+        assert.strictEqual(ns.toNumber("-2"), -2);
+    });
+    it('Negative () String', function () {
+        assert.strictEqual(ns.toNumber("(2)"), -2);
+    });
+    it('Other Chars', function () {
+        assert.strictEqual(ns.toNumber("a2g"), 2);
+    });
+    it('Single Decimal', function () {
+        assert.strictEqual(ns.toNumber("2.1"), 2.1);
+    });
+    it('Multiple Decimal', function () {
+        assert.strictEqual(ns.toNumber("2.1.2"), 21.2);
+    });
+    it('No Numbers', function () {
+        assert.ok(isNaN(ns.toNumber("asdf")));
+    });
+    it('Array', function () {
+        assert.ok(isNaN(ns.toNumber([1, 2, 3])));
+    });
+    it('Change Decimal Mark', function () {
+        assert.strictEqual(ns.toNumber("2,2", {decimalMark: ","}), 2.2);
+    });
+});
+
+describe('toClean', function () {
+    it('Positive Number', function () {
+        assert.strictEqual(ns.toClean(2.0), "2");
+    });
+    it('Negative Number', function () {
+        assert.strictEqual(ns.toClean(-2.0), "-2");
+    });
+    it('NaN', function () {
+        assert.ok(isNaN(ns.toClean(NaN)));
+    });
+    it('Infinity', function () {
+        assert.strictEqual(ns.toClean(Infinity), "Infinity");
+    });
+    it('-Infinity', function () {
+        assert.strictEqual(ns.toClean(-Infinity), "-Infinity");
+    });
+    it('Positive String', function () {
+        assert.strictEqual(ns.toClean("2.0"), "2");
+    });
+    it('Single Decimal', function () {
+        assert.strictEqual(ns.toClean("2.10"), "2.1");
+    });
+    it('Array', function () {
+        assert.strictEqual(ns.toClean([1, 2, 3]), "NaN");
+    });
+
+    it('Change Decimal Mark', function () {
+        assert.strictEqual(ns.toClean(1234.5, {decimalMark: ","}), "1,234,5");
+    });
+    it('Change Thousands Seperator', function () {
+        assert.strictEqual(ns.toClean(1234.5, { thousandSeperator: "." }), "1.234.5");
+    });
+    it('Change Max Precision', function () {
+        assert.strictEqual(ns.toClean(1234.5, { maxPrecision: 0 }), "1,235");
+    });
+    it('Change Min Precision', function () {
+        assert.strictEqual(ns.toClean(1234.5, { minPrecision: 3 }), "1,234.500");
+    });
+});
+
+describe('toMoney', function () {
+    it('Positive Number', function () {
+        assert.strictEqual(ns.toMoney(1234.5), "$1,234.50");
+    });
+    it('Negative Number', function () {
+        assert.strictEqual(ns.toMoney(-1234.5), "($1,234.50)");
+    });
+    //it('Negative Number Rounding Error', function () {
+    //    assert.strictEqual(ns.toMoney(-.005), "$0.00"); //FIXME: actual: "($0.01)"
+    //});
+    it('Positive String', function () {
+        assert.strictEqual(ns.toMoney("1234.5"), "$1,234.50");
+    });
+    it('Negative String', function () {
+        assert.strictEqual(ns.toMoney("-1234.5"), "($1,234.50)");
+    });
+    it('NaN', function () {
+        assert.strictEqual(ns.toMoney(NaN), "NaN");
+    });
+    it('Infinity', function () {
+        assert.strictEqual(ns.toMoney(Infinity), "Infinity");
+    });
+    it('-Infinity', function () {
+        assert.strictEqual(ns.toMoney(-Infinity), "(Infinity)");
+    });
+    it('Array', function () {
+        assert.strictEqual(ns.toMoney([1, 2, 3]), "NaN");
+    });
+    it('Change Decimal Mark', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { decimalMark: "," }), "$1,234,50");
+    });
+    it('Change Thousands Seperator', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { thousandSeperator: "." }), "$1.234.50");
+    });
+    it('Change Max Precision', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { maxPrecision: 0 }), "$1,235");
+    });
+    it('Change Min Precision', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { minPrecision: 3 }), "$1,234.500");
+    });
+    it('Change Symbol', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { symbol: "£" }), "£1,234.50");
+    });
+    it('Change Symbol Behind', function () {
+        assert.strictEqual(ns.toMoney(1234.5, { symbolBehind: true }), "1,234.50 $");
+    });
+
+});
+
+describe('toClosest', function () {
+    it('Positive Number', function () {
+        assert.strictEqual(ns.toClosest(1234.5, 5), 1235);
+    });
+    it('Positive Number Half Up', function () {
+        assert.strictEqual(ns.toClosest(1232.5, 5), 1235);
+    });
+    it('Negative Number', function () {
+        assert.strictEqual(ns.toClosest(-1234.5, 5), -1235);
+    });
+    it('Negative Number Half Up', function () {
+        assert.strictEqual(ns.toClosest(-1232.5, 5), -1230);
+    });
+    it('Decimal Number', function () {
+        assert.strictEqual(ns.toClosest(1.5, .4), 1.6);
+    });
+    //it('Floating Point Arithmetic Error', function () {
+    //    assert.strictEqual(ns.toClosest(1.5, .6), 1.8);// FIXME: actual: 1.7999999999999998
+    //});
+    it('Positive String', function () {
+        assert.strictEqual(ns.toClosest("1234.5", 5), 1235);
+    });
+    it('NaN', function () {
+        assert.ok(isNaN(ns.toClosest(NaN)));
+    });
+    it('Infinity', function () {
+        assert.strictEqual(ns.toClosest(Infinity, 5), Infinity);
+    });
+    it('-Infinity', function () {
+        assert.strictEqual(ns.toClosest(-Infinity, 5), -Infinity);
+    });
+    it('Array', function () {
+        assert.ok(isNaN(ns.toClosest([1, 2, 3], 1)));
+    });
+});
